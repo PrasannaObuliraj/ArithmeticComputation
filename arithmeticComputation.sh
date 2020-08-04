4f1a50a2f86ced5be83c6e94025eca38bf173cb0
unknown <press1998@gmail.com>
2020-08-04 08:20:01 +0530

    Added dictionary to store results of every computation

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -4,7 +4,13 @@ read -p "Enter number for a: " a
 read -p "Enter number for b: " b
 read -p "Enter number for c: " c
 
+declare -A operationDic
 operation1=$(($a+$b*$c))
 operation2=$(($a*$b+$c))
 operation3=$(($a+$b/$c))
 operation4=$(($a%$b+$c))
+
+for (( i=1 ; i<=4 ; i++ ))
+do
+	operationDic[$i]=$((operation$i))
+done
